8cb85cd0e10f67afd31795610b1470bd5477e8f2
Comzyh <comzyh@gmail.com>
2021-09-13 14:55:41 +0800

    Fix: ipv6 routeing in Tun

--- listener/tun/tunproxy.go
+++ listener/tun/tunproxy.go
@@ -79,7 +79,7 @@ func NewTunProxy(deviceURL string, tcpIn chan<- C.ConnContext, udpIn chan<- *inb
 	// So FindRoute will return correct route to tun NIC
 	subnet, _ := tcpip.NewSubnet(tcpip.Address(strings.Repeat("\x00", 4)), tcpip.AddressMask(strings.Repeat("\x00", 4)))
 	ipstack.AddRoute(tcpip.Route{Destination: subnet, Gateway: "", NIC: nicID})
-	subnet, _ = tcpip.NewSubnet(tcpip.Address(strings.Repeat("\x00", 6)), tcpip.AddressMask(strings.Repeat("\x00", 6)))
+	subnet, _ = tcpip.NewSubnet(tcpip.Address(strings.Repeat("\x00", 16)), tcpip.AddressMask(strings.Repeat("\x00", 16)))
 	ipstack.AddRoute(tcpip.Route{Destination: subnet, Gateway: "", NIC: nicID})
 
 	// TCP handler
